9b44d0a0b4d2908c8d31f313afa8caf49b03add1
Diogo Oliveira <diogo.ti@plasutil.com.br>
2015-10-20 10:30:11 -0200

    Adicionado alguns tipos de notificação.

--- app/src/main/java/com/diogo/mobile/app/util/Extras.java
+++ app/src/main/java/com/diogo/mobile/app/util/Extras.java
@@ -10,7 +10,7 @@ public class Extras
     public static String TAG = "GCM CLIENT HTTP";
     public static String SERVER_KEY = "AIzaSyAFtDM6-Q9xkEVsa7mTkw5XqrlaaKFOVV0";
     public static String SENDER_ID = "737179229350";
-    public static String SERVER = "http://172.16.7.38:8080/GCM_SERVER/resources/cliente";
+    public static String SERVER = "http://172.16.7.38:8080/GCM_SERVER_HTTP/resources/cliente";
 
     public static String PREFERENCES_STATUS = "PREFERENCES_STATUS";
 }
